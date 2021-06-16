528ebd6d387a44ecadf0fd32b2529a08a97ded48
Joe Muers <1000160+joemuers@users.noreply.github.com>
2021-06-16 18:21:18 +0100

    Initial Commit

---
+++ SwiftUIScrollView/SwiftUIScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollViewApp.swift
+//  SwiftUIScrollView
+//
+//  Created by joe muers on 16/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
